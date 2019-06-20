389313803def901475a2a05e7f12653758991ebf
yangchong211 <yangchong211@163.com>
2019-06-20 12:13:46 +0800

    初步完善自定义popupWindow封装

---
+++ example/lib/GroupList.dart
@@ -0,0 +1,127 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dialog/YcPopupWindow.dart';
+
+
+class GroupList extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new GroupState();
+  }
+}
+
+class GroupState extends State<GroupList>{
+
+  GlobalKey addKey = GlobalKey();
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("popupWindow案例"),
+        actions: <Widget>[
+          IconButton(
+            key: addKey,
+            onPressed: (){
+              showAddMenu();
+            },
+            icon: new Icon(Icons.add,),
+          ),
+        ],
+      ),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    Widget body = new Center(
+      child: new Text("popupWindow案例，点击右上角的按钮即可"),
+    );
+    return body;
+  }
+
+
+  void showAddMenu() {
+    final RenderBox button = addKey.currentContext.findRenderObject();
+    final RenderBox overlay = Overlay.of(context).context.findRenderObject();
+    var a =  button.localToGlobal(Offset(button.size.width - 8.0, button.size.height - 12.0), ancestor: overlay);
+    var b =  button.localToGlobal(button.size.bottomLeft(Offset(0, - 12.0)), ancestor: overlay);
+    final RelativeRect position = RelativeRect.fromRect(
+      Rect.fromPoints(a, b),
+      Offset.zero & overlay.size,
+    );
+    showPopupWindow(
+      context: context,
+      fullWidth: false,
+      isShowBg: true,
+      position: position,
+      elevation: 0.0,
+      child: InkWell(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onTap: (){
+
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 12.0),
+              child: Image.asset("lib/image/jt.png", width: 8.0, height: 4.0,),
+            ),
+            Container(
+              width: 120.0,
+              height: 40.0,
+              child: FlatButton.icon(
+                  onPressed: (){
+
+                  },
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(8.0), topRight: Radius.circular(8.0)),
+                  ),
+                  icon: new Icon(Icons.add_box),
+                  label: new Text("添加申请")
+              ),
+            ),
+            Container(width: 120.0, height: 0.6),
+            Container(
+              width: 120.0,
+              height: 40.0,
+              child: FlatButton.icon(
+                  color: Colors.white,
+                  onPressed: (){
+
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(8.0), bottomRight: Radius.circular(8.0)),
+                  ),
+                  icon: new Icon(Icons.report),
+                  label: new Text("拒绝申请")
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+
+
+
+
---
+++ example/lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dialog/YcToast.dart';
+import 'package:flutter_dialog_example/GroupList.dart';
+
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new AppState();
+  }
+}
+
+class AppState extends State<MyApp>{
+
+  final navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      navigatorKey: navigatorKey,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('小杨逗比弹窗工具库'),
+        ),
+        body: new ListView(
+          children: <Widget>[
+            new ListTile(
+              title: new Text("1.自定义popupWindow弹窗"),
+              onTap: (){
+                print("自定义popupWindow弹窗");
+                navigatorKey.currentState.push(
+                    new MaterialPageRoute(builder: (context) {
+                      return new GroupList();
+                    }));
+                /*Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => new GroupList(),
+                  ),
+                );*/
+              },
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("2.吐司工具，设置位置在中间"),
+              onTap: (){
+                YcToast.showToast(
+                    title: "逗比吐司",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.CENTER);
+              },
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("3.吐司工具，设置字体大小"),
+              onTap: (){
+                YcToast.showToast(
+                  title: "逗比",
+                  fontSize: 14,);
+              },
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("4.吐司工具，设置吐司圆角"),
+              onTap: (){
+                YcToast.showToast(
+                    title: "逗比",
+                    radius:25,
+                    gravity: ToastGravity.CENTER);
+              },
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("5.吐司工具，吐司标题和内容"),
+              onTap: (){
+                YcToast.showToast(
+                    title: "逗比",
+                    desc: "这个是内容",
+                    toastLength: Toast.LENGTH_SHORT,
+                    fontSize: 14,
+                    radius:20,
+                    gravity: ToastGravity.CENTER);
+              },
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("6.吐司工具，设置系统吐司"),
+              onTap: (){
+                YcToast.showToast(
+                    title: "逗比",
+                    type: Type.SYSTEM,
+                    toastLength: Toast.LENGTH_SHORT);
+              },
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("7.自定义吐司，设置背景和颜色"),
+              onTap: (){
+                YcToast.showToast(
+                  title: "逗比",
+                  desc: "这个是内容",
+                  toastLength: Toast.LENGTH_SHORT,
+                  fontSize: 14,
+                  radius:20,
+                  bgColor: Color(0xFFB71C1C),
+                  textColor: Color(0xFF4A148C),
+                  gravity: ToastGravity.CENTER,);
+              },
+            ),
+            new Divider(),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
